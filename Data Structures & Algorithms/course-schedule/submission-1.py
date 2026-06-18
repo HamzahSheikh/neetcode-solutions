ee5ffe185b6a6c03362e9b0edde76a622cb72bcc
Hamzah Sheikh <57862914+HamzahSheikh@users.noreply.github.com>
2026-06-17 23:56:34 -0400

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.py
@@ -0,0 +1,40 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+
+        courses = {i:[] for i in range(numCourses)}
+
+        for course, prereq in prerequisites:
+            courses[course].append(prereq)
+        
+        seen = set()
+        def courseSequencePossible(course) -> bool:
+            
+            if course in seen:
+                return False
+            
+            if courses[course] == []:
+                return True
+
+            seen.add(course)
+
+            for prereq in courses[course]:
+                if not courseSequencePossible(prereq):
+                    return False
+        
+            seen.remove(course)
+
+            return True
+
+
+        for course in courses:
+            print(course)
+            if not courseSequencePossible(course):
+                return False
+
+        return True
+
+            
+
+
+
+                
