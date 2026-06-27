fdbfd638ad46c003f581996c1ce1764a6d2dfa0d
Hamzah Sheikh <57862914+HamzahSheikh@users.noreply.github.com>
2026-06-27 14:33:06 -0400

    Add: path-with-minimum-effort - submission-4

---
+++ Data Structures & Algorithms/path-with-minimum-effort/submission-4.py
@@ -0,0 +1,61 @@
+class Solution:
+    
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+
+        def proccessNextNode(newRow, newCol):
+            newEffort = heights[newRow][newCol]
+            netEffort = abs(newEffort - currEffort)
+            netMaxEffort = max(netEffort, effort)
+            heapq.heappush(heap, (netMaxEffort, (newRow, newCol)))
+
+        start = (0, (0, 0))
+        maxRow = len(heights) - 1
+        maxCol = len(heights[0]) - 1
+
+        seen = set()
+
+        heap = []
+
+        heapq.heappush(heap, start)
+
+        while heap:
+
+            effort, coords = heapq.heappop(heap)
+            
+            if coords in seen:
+                continue
+            
+            seen.add(coords)
+
+            row = coords[0]
+            col = coords[1]
+            currEffort = heights[row][col]
+
+            if row == maxRow and col == maxCol:
+                return effort
+            
+            #right
+            if col < maxCol:
+                newRow, newCol = row, col+1
+                proccessNextNode(newRow, newCol)
+            
+            #left
+            if col > 0:
+                newRow, newCol = row, col-1
+                proccessNextNode(newRow, newCol)
+
+            #up
+            if row > 0:
+                newRow, newCol = row-1, col
+                proccessNextNode(newRow, newCol)
+
+            #down
+            if row < maxRow:
+                newRow, newCol = row+1, col
+                proccessNextNode(newRow, newCol)
+
+        return None
+
+            
+
+        
